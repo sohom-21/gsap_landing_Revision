b17b591991a3991c82fe1452642a73ccefd595d4
Sohom Ghosh <139136976+sohom-21@users.noreply.github.com>
2025-09-26 10:12:36 +0530

    Update contact information and address in Contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -41,13 +41,13 @@ const Contact = () => {
 
                 <div>
                     <h3>Visit Our Bar</h3>
-                    <p>456, Raq Blvd. #404, Los Angeles, CA 90210</p>
+                    <p>456, Raq Blvd. #404, Mumbai, Maharashtra, India 400001</p>
                 </div>
 
                 <div>
                     <h3>Contact Us</h3>
-                    <p>(555) 987-6543</p>
-                    <p>hello@jsmcocktail.com</p>
+                    <p>(+91) 98765 43210</p>
+                    <p>hello@NoblePourcocktail.in</p>
                 </div>
 
                 <div>
